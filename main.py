993ce12535ced40217f850cc835e882870524f46
Akhmad Ramadani <akhmdramadani18@gmail.com>
2023-06-17 17:57:16 +0700

    p

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template, jsonify
-# import numpy as np
-# from model import predictPothole
+import numpy as np
+from model import predictPothole
 import os
-# import cv2
+import cv2
 from multiprocessing import Value
 
 app = Flask(__name__)
@@ -13,41 +13,41 @@ counter = Value('i', 0)
 def hello_world():
     return 'Hi, It\'s me. I\'m the problem it\'s me.'
 
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     image = request.files['image'] # get the image
-#     # save the image to ./uploads
-#     image.save(os.path.join('./uploads', image.filename))
-#     # predict the class using path to image
-#     prediction = predictPothole(os.path.join('./uploads', image.filename))
-#     return jsonify({'prediction': prediction, })
+@app.route('/predict', methods=['POST'])
+def predict():
+    image = request.files['image'] # get the image
+    # save the image to ./uploads
+    image.save(os.path.join('./uploads', image.filename))
+    # predict the class using path to image
+    prediction = predictPothole(os.path.join('./uploads', image.filename))
+    return jsonify({'prediction': prediction, })
 
 
-# @app.route('/upload', methods=['POST','GET'])
-# def upload():
-# 	received = request
-# 	img = None
-# 	if received.files:
-# 		print(received.files['imageFile'])
-# 		# convert string of image data to uint8
-# 		file  = received.files['imageFile']
-# 		nparr = np.fromstring(file.read(), np.uint8)
-# 		# decode image
-# 		img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-# 		save_img(img)
+@app.route('/upload', methods=['POST','GET'])
+def upload():
+	received = request
+	img = None
+	if received.files:
+		print(received.files['imageFile'])
+		# convert string of image data to uint8
+		file  = received.files['imageFile']
+		nparr = np.fromstring(file.read(), np.uint8)
+		# decode image
+		img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+		save_img(img)
 		
-# 		return "[SUCCESS] Image Received", 201
-# 	else:
-# 		return "[FAILED] Image Not Received", 204
+		return "[SUCCESS] Image Received", 201
+	else:
+		return "[FAILED] Image Not Received", 204
 
-# def save_img(img):
-# 	with counter.get_lock():
-# 		counter.value += 1
-# 		count = counter.value
-# 	img_dir = "esp32_imgs"
-# 	if not os.path.isdir(img_dir):
-# 		os.mkdir(img_dir)
-# 	cv2.imwrite(os.path.join(img_dir,"img_"+str(count)+".jpg"), img)
+def save_img(img):
+	with counter.get_lock():
+		counter.value += 1
+		count = counter.value
+	img_dir = "esp32_imgs"
+	if not os.path.isdir(img_dir):
+		os.mkdir(img_dir)
+	cv2.imwrite(os.path.join(img_dir,"img_"+str(count)+".jpg"), img)
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=8000), host='0.0.0.0')
